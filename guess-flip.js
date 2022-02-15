8ab0430a7c8e06e95c0f9ed8c350fb1ebdfddff0
Stephen Buck <stephrb@ad.unc.edu>
2022-02-15 00:56:32 -0500

    test guess-flip

--- guess-flip.js
+++ guess-flip.js
@@ -4,5 +4,5 @@ const args = minimist(process.argv.slice(2));
 args["call"];
 const call = args.call;
 if (call == null) console.log("Error: no input.");
-else if (call != 'heads' && call != 'tails') console.log("Error: bad input.");
+//else if (call != 'heads' && call != 'tails') console.log("Error: bad input.");
 else console.log(flipACoin(call)); 
